e60d936b7c41896ec7fbd1d5fe36c4891d27f1e1
Liam Morgan <lite@getignited.co.uk>
2017-11-04 20:46:41 +0000

    Redefined Component as Functional Stateless Component

--- src/components/Laps/index.jsx
+++ src/components/Laps/index.jsx
@@ -1,39 +1,26 @@
-import React, { Component } from 'react';
-import TimeFormatter        from 'minutes-seconds-milliseconds';
+import React                from 'react'
+import TimeFormatter        from 'minutes-seconds-milliseconds'
 
-import './Laps.css';
-
-
-export default class Laps extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      displayOrder: [],
-    }
-  }
+import './Laps.css'
 
 
-  /**
-   * @function render
-   */
-  render() {
-
-    return(
-      <div className="Laps">
-        <ol>
-          {
-            this.props.data.map((lap, key) => {
-              return (
-                <li key={key}>
-                  <span className="Laps__number">Lap {key + 1}</span>
-                  <span className="Laps__time">{TimeFormatter(lap)}</span>
-                </li>
-              )
-            })
-          }
-        </ol>    
-      </div>
-    )
-  }
+function Laps({data}) {
+  return (
+    <ol className="list">
+      {data.map((lap, key) => {
+        return (
+          <li 
+            key={lap}
+            className="f--justify-space-between"
+          >
+            <span className="list__number">Lap {key + 1}</span>
+            <span className="list__time">{TimeFormatter(lap)}</span>
+          </li>
+        )
+      })}
+    </ol>
+  )
 }
+
+
+export default Laps
