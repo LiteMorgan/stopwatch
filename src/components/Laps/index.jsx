90f10f56f81fdfb16265b8617eb225419f15c152
Liam Morgan <lite@getignited.co.uk>
2017-11-04 20:47:59 +0000

    Changed 'laps' classes to 'List'

--- src/components/Laps/index.jsx
+++ src/components/Laps/index.jsx
@@ -6,15 +6,15 @@ import './Laps.css'
 
 function Laps({data}) {
   return (
-    <ol className="list">
+    <ol className="List">
       {data.map((lap, key) => {
         return (
           <li 
             key={lap}
             className="f--justify-space-between"
           >
-            <span className="list__number">Lap {key + 1}</span>
-            <span className="list__time">{TimeFormatter(lap)}</span>
+            <span className="List__number">Lap {key + 1}</span>
+            <span className="List__time">{TimeFormatter(lap)}</span>
           </li>
         )
       })}
