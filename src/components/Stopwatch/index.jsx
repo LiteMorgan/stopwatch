93dd8a725f84153ef97d4faf78bd346a84e43199
Liam Morgan <lite@getignited.co.uk>
2017-11-04 20:02:32 +0000

    Redefined component as Functional Stateless Component

--- src/components/Stopwatch/index.jsx
+++ src/components/Stopwatch/index.jsx
@@ -1,31 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Stopwatch.css';
 
 
-export default class Stopwatch extends Component {
-  /**
-   * @function constructor
-   */
-  // constructor(props) {
-  //   super(props);
-  // };
-
-  /**
-   * @function render
-   */
-  render() {
-
-    return(
-      <div className="Stopwatch">
-        <div className="Stopwatch__clocks">
-          <div className="Stopwatch__main">
-            {this.props.mainTimer}
-          </div>
-          <div className="Stopwatch__lap">
-            <span className="Stopwatch__prelap">Lap time:</span> {this.props.lapTimer}
-          </div>
-        </div>
+function Stopwatch({mainTimer, lapTimer}) {
+  return (
+    <div className="Stopwatch__clocks">
+      <div className="Stopwatch__main">{mainTimer}</div>
+      <div className="Stopwatch__lap">
+        <span className="Stopwatch__prelap">Lap time: </span>
+        {lapTimer}
       </div>
-    )
-  }
-}
+    </div>
+  )
+};
+
+
+export default Stopwatch;
